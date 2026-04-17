9cb198b34e09d303e13e13255e9ed36053d733ad
mberlins <mberlins@outlook.com>
2026-04-17 03:29:01 -0400

    package created.

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=input_json, headers=headers)
+    formatted_res = json.loads(response.text)
+    emotions = formatted_res['emotionPredictions'][0]['emotion']
+    max_key = max(emotions, key=emotions.get)
+    res = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': max_key
+    }
+    return res
