b57937923b040e95f14fa92d23363f130fc53987
mberlins <mberlins@outlook.com>
2026-04-17 04:33:10 -0400

    Server added.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,15 +6,36 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json=input_json, headers=headers)
-    formatted_res = json.loads(response.text)
-    emotions = formatted_res['emotionPredictions'][0]['emotion']
-    max_key = max(emotions, key=emotions.get)
-    res = {
-        'anger': emotions['anger'],
-        'disgust': emotions['disgust'],
-        'fear': emotions['fear'],
-        'joy': emotions['joy'],
-        'sadness': emotions['sadness'],
-        'dominant_emotion': max_key
-    }
+
+    if response.status_code == 200: 
+        formatted_res = json.loads(response.text)
+        emotions = formatted_res['emotionPredictions'][0]['emotion']
+        max_key = max(emotions, key=emotions.get)
+        res = {
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': max_key
+        }
+    elif response.status_code == 500:
+        res = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    else:
+        res = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        
     return res
